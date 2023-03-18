64969694f97fc9471241d377269c63662160a931
Yiddies <chrislicks104@gmail.com>
2023-03-17 22:59:44 -0400

    I forgot. got most of it working tbh just some minor changes, got to implement a save featue

---
+++ game/main.rb
@@ -0,0 +1,61 @@
+HANGMAN_WORDS = File.readlines("../google-10000-english-no-swears.txt")
+
+class HangMan
+  def initialize
+    @word = ""
+    @guesses_left = 10
+  end
+
+  def start_game
+    puts "Welcome to HangMan!"
+    puts "Press 1 to start a new game"
+    puts "Press 2 to load a game"
+    input = gets.chomp
+
+    if input == "1"
+      @word = get_word(HANGMAN_WORDS)
+      return play_game
+    elsif input == "2"
+      puts "test"
+    end
+  end
+
+  def get_word(hangman_words)
+    random_word = hangman_words.sample.chomp
+    if random_word.length >= 5
+      @word = random_word
+    else
+      return get_word(hangman_words)
+    end
+  end
+
+  def play_game
+    puts @word
+    word_hidden = "_" * @word.length
+    puts word_hidden
+    while @guesses_left > 0 && word_hidden != @word
+      puts "#{@guesses_left} Guessess left"
+      puts "Guess a letter:"
+      guess = gets.chomp
+      found = false
+      @word.each_char.with_index do |char, i|
+        if char == guess
+          word_hidden[i] = guess
+          found = true
+        end
+      end
+      if word_hidden == @word
+        puts "Correct"
+      else
+        @guesses_left -= 1
+      end
+      puts word_hidden
+    end
+    if @guesses_left == 0
+      puts "Game Over!"
+      puts "The word was #{@word}"
+    end
+  end
+end
+
+HangMan.new.start_game
